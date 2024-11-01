c46c1850955a8df1917e5c0359deafda17f83a6c
neumann.niklas <neumann.niklas98@gmail.com>
2024-11-01 16:57:56 +0100

    muscles testing setup

--- src/muscles/muscles.controller.spec.ts
+++ src/muscles/muscles.controller.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Muscle } from './entities/muscle.entity';
 import { MusclesController } from './muscles.controller';
 import { MusclesService } from './muscles.service';
 
 describe('MusclesController', () => {
-  let controller: MusclesController;
+  let musclesController: MusclesController;
+  let musclesService: MusclesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,10 +13,22 @@ describe('MusclesController', () => {
       providers: [MusclesService]
     }).compile();
 
-    controller = module.get<MusclesController>(MusclesController);
+    musclesController = module.get<MusclesController>(MusclesController);
+    musclesService = module.get<MusclesService>(MusclesService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(musclesController).toBeDefined();
+    expect(musclesService).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of muscles', async () => {
+      const muscles: Muscle[] = [];
+
+      jest.spyOn(musclesService, 'findAll').mockImplementation(async () => muscles);
+
+      expect(await musclesController.findAll()).toBe(muscles);
+    });
   });
 });
--- src/muscles/muscles.service.spec.ts
+++ src/muscles/muscles.service.spec.ts
@@ -1,18 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Muscle } from './entities/muscle.entity';
 import { MusclesService } from './muscles.service';
 
 describe('MusclesService', () => {
-  let service: MusclesService;
+  let musclesService: MusclesService;
+  let musclesRepository: Repository<Muscle>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MusclesService]
+      providers: [
+        MusclesService,
+        { provide: getRepositoryToken(Muscle), useClass: Repository<Muscle> }
+      ]
     }).compile();
 
-    service = module.get<MusclesService>(MusclesService);
+    musclesService = module.get<MusclesService>(MusclesService);
+    musclesRepository = module.get(getRepositoryToken(Muscle));
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(musclesService).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of muscles', async () => {
+      const muscles: Muscle[] = [];
+
+      jest.spyOn(musclesRepository, 'find').mockImplementation(async () => muscles);
+
+      expect(await musclesService.findAll()).toBe(muscles);
+    });
   });
 });
